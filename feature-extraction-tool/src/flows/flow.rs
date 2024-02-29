7573c5d961cb0d3a14a05c37a058742b57c6f412
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-02-29 20:51:29 +0100

    :sparkles: Add flow trait

--- feature-extraction-tool/src/flows/flow.rs
+++ feature-extraction-tool/src/flows/flow.rs
@@ -1,8 +1,8 @@
 use std::time::Instant;
-use chrono::{DateTime, Utc};
+
 use common::BasicFeatures;
 
 pub trait Flow {
-    fn update_flow(&mut self, packet: BasicFeatures, fwd: bool);
-    fn print(&self);
-} 
+    fn update_flow(&mut self, packet: BasicFeatures, timestamp: Instant, fwd: bool);
+    fn update_flow_first(&mut self, packet: BasicFeatures, timestamp: Instant, fwd: bool);
+}
