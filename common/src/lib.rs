781d10a049a8afb1b2c27d19078ce3892735826d
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-02-29 20:52:06 +0100

    :card_file_box: Update ebpf payload struct

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,13 +1,25 @@
 #![no_std]
-
+// BasicFeatures is 264 bits in size
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct PacketLog {
+pub struct BasicFeatures {
     pub ipv4_destination: u32,
     pub ipv4_source: u32,
     pub port_destination: u16,
     pub port_source: u16,
+    pub protocol: u8,
+    pub fin_flag: u8,
+    pub syn_flag: u8,
+    pub rst_flag: u8,
+    pub psh_flag: u8,
+    pub ack_flag: u8,
+    pub urg_flag: u8,
+    pub cwe_flag: u8,
+    pub ece_flag: u8,
+    pub data_length: u32,
+    pub header_length: u32,
+    pub length: u32,
 }
 
 #[cfg(feature = "user")]
-unsafe impl aya::Pod for PacketLog {}
+unsafe impl aya::Pod for BasicFeatures {}
