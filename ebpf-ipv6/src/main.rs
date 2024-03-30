c645b70703aede16079821ac2ef84d5aad040fe2
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-03-30 14:31:09 +0100

    :art: Update structure

--- ebpf-ipv6/src/main.rs
+++ ebpf-ipv6/src/main.rs
@@ -10,7 +10,6 @@ use aya_ebpf::{
 };
 
 use common::BasicFeaturesIpv6;
-
 use core::mem;
 use network_types::{
     eth::{EthHdr, EtherType},
@@ -29,129 +28,129 @@ static EVENTS_IPV6: PerfEventArray<BasicFeaturesIpv6> = PerfEventArray::with_max
 
 #[classifier]
 pub fn tc_flow_track(ctx: TcContext) -> i32 {
-    match try_tc_flow_track(ctx) {
-        Ok(ret) => ret,
+    match process_packet(&ctx) {
+        Ok(action) => action,
         Err(_) => TC_ACT_PIPE,
     }
 }
 
-#[inline(always)]
-unsafe fn ptr_at<T>(ctx: &TcContext, offset: usize) -> Result<*const T, ()> {
-    let start = ctx.data();
-    let end = ctx.data_end();
-    let len = mem::size_of::<T>();
-
-    if start + offset + len > end {
-        return Err(());
+fn process_packet(ctx: &TcContext) -> Result<i32, ()> {
+    let ethhdr = ctx.load::<EthHdr>(0).map_err(|_| ())?;
+    match ethhdr.ether_type {
+        EtherType::Ipv6 => process_ipv6_packet(ctx),
+        _ => Ok(TC_ACT_PIPE),
     }
-
-    let ptr = (start + offset) as *const T;
-    Ok(&*ptr)
 }
 
-fn try_tc_flow_track(ctx: TcContext) -> Result<i32, ()> {
-    let ethhdr: *const EthHdr = unsafe { ptr_at(&ctx, 0)? };
-    match unsafe { (*ethhdr).ether_type } {
-        EtherType::Ipv6 => {}
-        _ => return Ok(TC_ACT_PIPE),
+fn process_ipv6_packet(ctx: &TcContext) -> Result<i32, ()> {
+    let ipv6hdr = ctx.load::<Ipv6Hdr>(EthHdr::LEN).map_err(|_| ())?;
+    let packet_info = PacketInfo::new(&ipv6hdr, ctx.data_end() - ctx.data())?;
+
+    match ipv6hdr.next_hdr {
+        IpProto::Tcp => process_tcp_packet(ctx, packet_info),
+        IpProto::Udp => process_udp_packet(ctx, packet_info),
+        _ => Ok(TC_ACT_PIPE),
+    }
+}
+
+fn process_tcp_packet(ctx: &TcContext, packet_info: PacketInfo) -> Result<i32, ()> {
+    let tcphdr = ctx
+        .load::<TcpHdr>(EthHdr::LEN + Ipv6Hdr::LEN)
+        .map_err(|_| ())?;
+    let packet_log = packet_info.to_packet_log(&tcphdr);
+    EVENTS_IPV6.output(ctx, &packet_log, 0);
+    Ok(TC_ACT_PIPE)
+}
+
+fn process_udp_packet(ctx: &TcContext, packet_info: PacketInfo) -> Result<i32, ()> {
+    let udphdr = ctx
+        .load::<UdpHdr>(EthHdr::LEN + Ipv6Hdr::LEN)
+        .map_err(|_| ())?;
+    let packet_log = packet_info.to_packet_log(&udphdr);
+    EVENTS_IPV6.output(ctx, &packet_log, 0);
+    Ok(TC_ACT_PIPE)
+}
+
+struct PacketInfo {
+    ipv6_source: u128,
+    ipv6_destination: u128,
+    data_length: u16,
+    protocol: u8,
+}
+
+impl PacketInfo {
+    fn new(ipv6hdr: &Ipv6Hdr, data_length: usize) -> Result<Self, ()> {
+        Ok(Self {
+            ipv6_source: u128::from_be_bytes(unsafe { ipv6hdr.src_addr.in6_u.u6_addr8 }),
+            ipv6_destination: u128::from_be_bytes(unsafe { ipv6hdr.dst_addr.in6_u.u6_addr8 }),
+            data_length: data_length as u16,
+            protocol: ipv6hdr.next_hdr as u8,
+        })
     }
 
-    let ipv6hdr: *const Ipv6Hdr = unsafe { ptr_at(&ctx, EthHdr::LEN)? };
-    let bytes_ipv6_source = unsafe { (*ipv6hdr).src_addr.in6_u.u6_addr8 };
-    let bytes_ipv6_destination = unsafe { (*ipv6hdr).dst_addr.in6_u.u6_addr8 };
+    fn to_packet_log<T: NetworkHeader>(&self, header: &T) -> BasicFeaturesIpv6 {
+        BasicFeaturesIpv6::new(
+            self.ipv6_destination,
+            self.ipv6_source,
+            header.destination_port(),
+            header.source_port(),
+            self.data_length,
+            self.data_length + header.header_length() as u16,
+            header.window_size(),
+            header.combined_flags(),
+            self.protocol,
+            header.header_length(),
+        )
+    }
+}
 
-    let ipv6_source = u128::from_be_bytes(bytes_ipv6_source);
-    let ipv6_destination = u128::from_be_bytes(bytes_ipv6_destination);
+trait NetworkHeader {
+    fn source_port(&self) -> u16;
+    fn destination_port(&self) -> u16;
+    fn window_size(&self) -> u16;
+    fn combined_flags(&self) -> u8;
+    fn header_length(&self) -> u8;
+}
 
-    let source_port: u16;
-    let destination_port: u16;
+impl NetworkHeader for TcpHdr {
+    fn source_port(&self) -> u16 {
+        self.source
+    }
+    fn destination_port(&self) -> u16 {
+        self.dest
+    }
+    fn window_size(&self) -> u16 {
+        self.window as u16
+    }
+    fn combined_flags(&self) -> u8 {
+        ((self.fin() as u8) << 0)
+            | ((self.syn() as u8) << 1)
+            | ((self.rst() as u8) << 2)
+            | ((self.psh() as u8) << 3)
+            | ((self.ack() as u8) << 4)
+            | ((self.urg() as u8) << 5)
+            | ((self.ece() as u8) << 6)
+            | ((self.cwr() as u8) << 7)
+    }
+    fn header_length(&self) -> u8 {
+        TcpHdr::LEN as u8
+    }
+}
 
-    let protocol: u8;
-
-    let mut fin_flag_count = 0 as u8;
-    let mut syn_flag_count = 0 as u8;
-    let mut rst_flag_count = 0 as u8;
-    let mut psh_flag_count = 0 as u8;
-    let mut ack_flag_count = 0 as u8;
-    let mut urg_flag_count = 0 as u8;
-    let mut cwe_flag_count = 0 as u8;
-    let mut ece_flag_count = 0 as u8;
-
-    let header_length: u32;
-    let data_length: usize = ctx.data_end() - ctx.data();
-    let length: u32;
-    let mut window_size: u16 = 0;
-
-    match unsafe { *ipv6hdr }.next_hdr {
-        IpProto::Tcp => {
-            let tcphdr: *const TcpHdr = unsafe { ptr_at(&ctx, EthHdr::LEN + Ipv6Hdr::LEN) }?;
-
-            // Cast the `TcpHdr` pointer to a `*const u8` to read individual bytes
-            let tcphdr_u8 = tcphdr as *const u8;
-            // Read the 12th byte (offset 12 from the TCP header start)
-            let data_offset_byte = unsafe { *tcphdr_u8.add(12) } as u8;
-            // Extract the high-order 4 bits and shift right by 4 to get the 'data offset' value
-            let data_offset = (data_offset_byte >> 4) as u32;
-            // Calculate the TCP header size in bytes
-            header_length = data_offset * 4;
-
-            length = data_length as u32
-                + header_length as u32
-                + Ipv6Hdr::LEN as u32
-                + EthHdr::LEN as u32;
-
-            source_port = u16::from_be(unsafe { *tcphdr }.source);
-            destination_port = u16::from_be(unsafe { *tcphdr }.dest);
-
-            fin_flag_count = (unsafe { *tcphdr }.fin() != 0) as u8;
-            syn_flag_count = (unsafe { *tcphdr }.syn() != 0) as u8;
-            rst_flag_count = (unsafe { *tcphdr }.rst() != 0) as u8;
-            psh_flag_count = (unsafe { *tcphdr }.psh() != 0) as u8;
-            ack_flag_count = (unsafe { *tcphdr }.ack() != 0) as u8;
-            urg_flag_count = (unsafe { *tcphdr }.urg() != 0) as u8;
-            cwe_flag_count = (unsafe { *tcphdr }.cwr() != 0) as u8;
-            ece_flag_count = (unsafe { *tcphdr }.ece() != 0) as u8;
-
-            protocol = IpProto::Tcp as u8;
-            window_size = u16::from_be(unsafe { *tcphdr }.window);
-        }
-        IpProto::Udp => {
-            let udphdr: *const UdpHdr = unsafe { ptr_at(&ctx, EthHdr::LEN + Ipv6Hdr::LEN) }?;
-            source_port = u16::from_be(unsafe { *udphdr }.source);
-            destination_port = u16::from_be(unsafe { *udphdr }.dest);
-
-            header_length = UdpHdr::LEN as u32;
-            length = data_length as u32
-                + header_length as u32
-                + Ipv6Hdr::LEN as u32
-                + EthHdr::LEN as u32;
-            protocol = IpProto::Udp as u8;
-        }
-        _ => return Ok(TC_ACT_PIPE),
-    };
-
-    let flow = BasicFeaturesIpv6 {
-        ipv6_destination: ipv6_destination,
-        ipv6_source: ipv6_source,
-        port_destination: destination_port,
-        port_source: source_port,
-        protocol: protocol,
-        fin_flag: fin_flag_count,
-        syn_flag: syn_flag_count,
-        rst_flag: rst_flag_count,
-        psh_flag: psh_flag_count,
-        ack_flag: ack_flag_count,
-        urg_flag: urg_flag_count,
-        cwe_flag: cwe_flag_count,
-        ece_flag: ece_flag_count,
-        data_length: data_length as u32,
-        header_length: header_length,
-        length: length,
-        window_size: window_size,
-    };
-
-    // the zero value is a flag
-    EVENTS_IPV6.output(&ctx, &flow, 0);
-
-    Ok(TC_ACT_PIPE)
+impl NetworkHeader for UdpHdr {
+    fn source_port(&self) -> u16 {
+        self.source
+    }
+    fn destination_port(&self) -> u16 {
+        self.dest
+    }
+    fn window_size(&self) -> u16 {
+        0
+    }
+    fn combined_flags(&self) -> u8 {
+        0
+    }
+    fn header_length(&self) -> u8 {
+        UdpHdr::LEN as u8
+    }
 }
